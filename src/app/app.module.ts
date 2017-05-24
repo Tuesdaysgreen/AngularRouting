ebbcbb510afb69543ec5260a2bc8cb4be512d2a4
Elliott Hamai <ellhamai@microsoft.com>
2017-05-24 14:36:38 -0700

    New module without lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
+import { ChildModule } from './child/child.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { InitialComponent } from './initial/initial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialComponent,
+    // ChildComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ChildModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/initial/initial.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-initial',
+  templateUrl: './initial.component.html',
+  styleUrls: ['./initial.component.css']
+})
+export class InitialComponent implements OnInit {
+
+  title = "Initial component";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
